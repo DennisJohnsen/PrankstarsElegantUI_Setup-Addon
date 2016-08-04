769f8830ceb2b0fcc75aec1692de9b1cf84ea7d6
DennisJohnsen <dennis.k.johnsen@gmail.com>
2016-08-04 02:30:15 +0200

    Add files via upload

--- PrankstarsElegantUI.lua
+++ PrankstarsElegantUI.lua
@@ -2,29 +2,60 @@
 -- Prankstars Elegant UI - Namespaces and variables
 ]]--
 PrankUI = "PrankUI";
+ProfileName = "Prankstars Elegant UI";
 MediaPath = "Interface\\AddOns\\PrankstarsElegantUI\\media\\";
-FrameStrata = "HIGH"; -- BACKGROUND, LOW, MEDIUM, HIGH, DIALOG, FULLSCREEN, FULLSCREEN_DIALOG, TOOLTIP
+FrameStrata = "MEDIUM"; -- BACKGROUND, LOW, MEDIUM, HIGH, DIALOG, FULLSCREEN, FULLSCREEN_DIALOG, TOOLTIP
 
 
 --[[
     Lists of TODO:
-    - The setup window
-        - Background, with two buttons that has an hover effect behind the two options
-
-    - Some kind of way to open the setup window when typing "/prankui"
-
-    - Simple and easy way of adding profiles/change addon db var when an style is clicked
-        - Also reloads UI after
-
-    - When running /prankui on a toon that allready has the UI setup, it should be able to click the same, or a different style.
+    - Masque Profile
+    - Move Anything Profile
+    - Tidy plates paste settings from clipboard
+    - Enable all actionbars (WOW)
 ]]--
 
 --[[
-    Prankstars Elegant UI - Setup window
+    Profiles
+]]--
+local kgPanels = LibStub("AceAddon-3.0"):GetAddon("kgPanels");
+local Castbars = LibStub("AceAddon-3.0"):GetAddon("Castbars");
+local Masque = LibStub("AceAddon-3.0"):GetAddon("Masque");
+
+function profileDB (addon, addonLayout)
+    addon.db:SetProfile(UnitName("player").." - "..GetRealmName());
+
+    if addonLayout == "ironhorde" then
+        addon.db:CopyProfile(ProfileName.." - Iron Horde", true);
+    elseif addonLayout == "legion" then
+        addon.db:CopyProfile(ProfileName.." - Legion", true);
+    else
+        addon.db:CopyProfile(ProfileName, true);
+    end
+end
+
+function profileSetup (layout)
+    profileDB(Bartender4);
+    profileDB(ShadowUF);
+    profileDB(Skada);
+    profileDB(Prat);
+    profileDB(Castbars, layout);
+    profileDB(kgPanels, layout);
+
+    SexyMap2DB[(UnitName("player").."-"..GetRealmName())] = "global";
+
+    ReloadUI();
+end
+
+
+
+
+--[[
+    Setup window
 ]]--
 
 -- Main frame
-local UISetup = CreateFrame("Frame", PrankUI, UIParent);
+UISetup = CreateFrame("Frame", PrankUI, UIParent);
 UISetup:SetFrameStrata(FrameStrata);
 UISetup:SetSize(1024, 256); -- W, H
 UISetup:SetPoint("CENTER", UIParent, "CENTER", 0, 128); -- point, relative frame, relative point, x, y
@@ -34,8 +65,7 @@ UISetup.bg = UISetup:CreateTexture(PrankUI..".bg", "BACKGROUND");
 UISetup.bg:SetAllPoints();
 UISetup.bg:SetTexture(MediaPath.."background.tga");
 
-
-
+ 
 -- Seperator
 UISetup.seperator = CreateFrame("Frame", PrankUI..".seperator", UISetup);
 UISetup.seperator:SetSize(128, 128);
@@ -45,7 +75,6 @@ UISetup.seperator:SetFrameLevel(4);
 UISetup.seperator.bg = UISetup.seperator:CreateTexture(PrankUI..".seperator.bg", "BACKGROUND");
 UISetup.seperator.bg:SetAllPoints();
 UISetup.seperator.bg:SetTexture(MediaPath.."seperator.tga"); 
-
 
 
 -- Iron Horde Option
@@ -80,9 +109,9 @@ UISetup.ironhorde.button:SetScript('OnLeave', function()
     UISetup.ironhorde.hover:SetAlpha(0);
 end);
 UISetup.ironhorde.button:SetScript("OnClick", function()
-    print("Clicked Iron Horde");
+    print("Iron Horde layout selected!");
+    profileSetup("ironhorde");
 end);
-
 
 
 -- Legion Option
@@ -117,5 +146,23 @@ UISetup.legion.button:SetScript('OnLeave', function()
     UISetup.legion.hover:SetAlpha(0);
 end);
 UISetup.legion.button:SetScript("OnClick", function()
-    print("Clicked Legion");
+    print("Legion layout selected!");
+    profileSetup("legion");
 end);
+
+
+-- Hide main frame
+UISetup:Hide();
+
+
+
+
+--[[
+    Slash Command
+]]--
+
+-- Open setup window 
+SLASH_PRANKUI1, SLASH_PRANKUI2 = '/prankui', '/pui';
+function SlashCmdList.PRANKUI(msg, editBox)
+    UISetup:Show();
+end
